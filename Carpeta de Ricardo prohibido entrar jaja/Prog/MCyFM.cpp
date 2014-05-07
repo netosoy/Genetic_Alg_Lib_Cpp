58c9a13b39db3609d03c17c419bac211a79a48c7
rchaconca <vrlrdo@gmail.com>
2014-05-07 09:44:43 -0600

    be

---
+++ Carpeta de Ricardo prohibido entrar jaja/Prog/MCyFM.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+#include <math.h>
+
+
+
+    // Crossover individuals
+    private static Individual crossover(Individual indiv1, Individual indiv2) {
+        Individual newSol = new Individual();
+        // Loop through genes
+        for (int i = 0; i < indiv1.size(); i++) {
+            // Crossover
+            if (Math.random() <= uniformRate) {
+                newSol.setGene(i, indiv1.getGene(i));
+            } else {
+                newSol.setGene(i, indiv2.getGene(i));
+            }
+        }
+        return newSol;
+    }
+    
+    
+     private static Individual tournamentSelection(Population pop) {
+        // Create a tournament population
+        Population tournament = new Population(tournamentSize, false);
+        // For each place in the tournament get a random individual
+        for (int i = 0; i < tournamentSize; i++) {
+            int randomId = (int) (Math.random() * pop.size());
+            tournament.saveIndividual(i, pop.getIndividual(randomId));
+        }
+        // Get the fittest
+        Individual fittest = tournament.getFittest();
+        return fittest;
+    }
