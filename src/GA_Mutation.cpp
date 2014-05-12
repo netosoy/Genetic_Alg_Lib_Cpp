d29bd5672cc975655c5da628164da384e8eeb83e
ejcerdesmo <netosoy@gmail.com>
2014-05-12 12:07:28 -0600

    include ans src directories

---
+++ src/GA_Mutation.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+//#include "Platform.hh"
+//#include "Threading.hh"
+//#include "Random.hh"
+
+#define VNAME(x)  #x 
+
+using namespace std;
+int randomInt(int, int);
+void print_vector(vector<int> &);
+void fill_vector(vector<int> &);
+void swapPos(int, int,vector<int> &);
+void GaInvertMutation(vector<int> &, int);
+
+//vector<int> A(7);
+vector<int> B(7);
+
+int main(void){
+	vector<int> A(7);
+	print_vector(A);
+	fill_vector(A);
+	GaInvertMutation(A,3);
+	cout<<"the muted vector A"<<endl;
+	print_vector(A);
+	swapPos(1,6,A);
+	cout<<"the swaped vector A"<<endl;
+	print_vector(A);
+	}
+	
+int randomInt(int min, int max){
+	int output = min + (rand() % (int)(max - min));
+	return output;
+	}	
+
+void print_vector(vector<int> &V){
+	cout<<"The vector "<<VNAME(V)<<":"<<endl<<"[";
+	for(int i=0;i<V.size();i++){
+		cout<<V[i]<<" ";
+		}
+	cout<<"]"<<endl;
+	}
+
+void fill_vector(vector<int> &V){
+	cout<<"enter the values of the vector"<<endl;
+	for(int i=0;i<V.size();i++){
+		cin>>V[i];
+		}
+	}
+void GaInvertMutation(vector<int> &V, int n){
+	for(int i=0;i<n;i++){
+		//cout<<"generando numero aleatorio: "<<randomInt(0,V.size())<<endl;
+		V[randomInt(0,V.size())]*=-1;
+		}
+	}
+void swapPos(int i, int j, vector<int> &V){
+	int aux = V[i];
+	V[i]=V[j];
+	V[j]=aux;
+	}
+
