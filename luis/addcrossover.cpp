cf37b509b3eb4cbf891d3ae57fe6740edf9941df
lsyannicelli <lsyannicelli@gmail.com>
2014-05-17 11:47:39 -0600

    addcrossover casi listo

--- luis/addcrossover.cpp
+++ luis/addcrossover.cpp
@@ -8,14 +8,19 @@
 using namespace std;
 using std::string;
 
-//------------------------variables declaration-----------------------------------------
-int parent1 [10];
-int parent2 [10];
-int son [10];
-
 
 int main(void){
 
+//------------------------variables declaration-----------------------------------------
+int parent1 [10];
+int *ptr1;
+ptr1=parent1;
+int parent2 [10];
+int *ptr2;
+ptr2=parent2;
+int son [10];
+//int *ptrS;
+//ptrS=son;
 
 //--------------------------parent1-----------------------------------------------------
 cout << "Enter values for the 10 sized vector parent1 on position: " << ": "<< endl;
@@ -44,25 +49,29 @@ for(int i=0;i<10;i++){
  }
 cout << "]" << "\n\n" << endl;
 
-//-------------------------sum method------------------------------------------------
+//------------------------random selection------------------------------
 
+//-------------------------sum method-----------------------------------
 
-int *ptr1;
-ptr1 = parent1;
-int *ptr2;
-ptr2 = parent2;
-int num
-
-for (int i =0; i<9; i++){
-	num = &ptr1[i] + &ptr2[i];
+for(vector<int>::size_type i=0;i<10;i++){
+	son[i]=ptr1[i]+ptr2[i];
 }
 
-for(int i = 0; i < 9; i++){   
-	
-	son[i]=
-	}
+cout << "Son vector is:" << endl;
+for(int i=0;i<10;i++){
+	cout << son[i] << ",";
+ }
+cout << "]" << "\n\n" << endl;
 
+//-------------------------minus method---------------------------------
+for(vector<int>::size_type i=0;i<10;i++){
+	son[i]=ptr1[i]-ptr2[i];
+}
 
-
+cout << "Son vector is:" << endl;
+for(int i=0;i<10;i++){
+	cout << son[i] << ",";
+ }
+cout << "]" << "\n\n" << endl;
 return 0;
 }
